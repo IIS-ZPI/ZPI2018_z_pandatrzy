1db5b85118b5c18dd77196db71a2481f97760a36
Hapti <pk.hapti@gmail.com>
2018-12-16 11:32:22 +0100

    Spring & Springboot removal - simplifying application

--- src/main/java/pl/panda/trzy/application/Application.java
+++ src/main/java/pl/panda/trzy/application/Application.java
@@ -1,19 +1,9 @@
 package pl.panda.trzy.application;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.ComponentScan;
-
-@SpringBootApplication
-@ComponentScan(basePackages = "pl.panda.trzy")
-@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
-public class Application extends SpringBootServletInitializer {
+public class Application {
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        for (String s: args) {
+            System.out.println(s);
+        }
     }
 }
