136971fedbf4ca1c7fc0295dbb25e8892f0bec69
ancalentari <andre45@vp.pl>
2018-12-16 20:52:52 +0100

    update nbp interface

--- src/main/java/pl/panda/trzy/nbp/NbpApi.java
+++ src/main/java/pl/panda/trzy/nbp/NbpApi.java
@@ -4,6 +4,20 @@ import java.util.Map;
 import java.util.Set;
 
 public interface NbpApi {
+    /**
+     * Pobiera kurs pojedynczej waluty dla wybranego okresu
+     * @param currency waluta
+     * @param periodType wybrany okres
+     * @return kurs waluty
+     */
     NbpResponse getExchangeRate(String currency, PeriodType periodType);
-    Map<String, NbpResponse> getExchangeRates(Set<String> currency, PeriodType periodType);
+
+    /**
+     * Pobiera kursy dla wielu walut dla wybranego okresu. Działa wielowątkowo, kurs każdej waluty pobierany jest
+     * w osobnym wątku
+     * @param currencies kolekcja walut
+     * @param periodType wybrany okres
+     * @return mapa kursów walut
+     */
+    Map<String, NbpResponse> getExchangeRates(Set<String> currencies, PeriodType periodType);
 }
